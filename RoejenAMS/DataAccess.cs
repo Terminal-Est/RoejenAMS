ae623cde7fc1a21b14e22fede4278fde2992ab39
Terminal-Est <cstephen1982@gmail.com>
2020-10-15 00:06:49 +1100

    Add project files.

---
+++ RoejenAMS/DataAccess.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+
+namespace RoejenAMS
+{
+    class DataAccess
+    {
+        public string SelectQuery()
+        {
+            string query = "";
+            using (var con = new SqliteConnection("Data Source=RoejenAMS.db"))
+            {
+                con.Open();
+
+                var command = con.CreateCommand();
+                command.CommandText = @"
+                                    SELECT name
+                                    FROM user
+                                    WHERE id = $id
+                                      ";
+                command.Parameters.AddWithValue("$id", 1);
+
+                using (var reader = command.ExecuteReader())
+                    while (reader.Read())
+                    {
+                        var name = reader.GetString(0);
+
+                        string helloGet = ($"Hello, {name}!");
+                        query = helloGet;
+                    }
+            }
+
+            return query;
+        }
+
+        public void CreateTable()
+        {
+            using (var con = new SqliteConnection("Data Source=RoejenAMS.db"))
+            {
+                con.Open();
+                string commandText =  @"DROP TABLE IF EXISTS test; CREATE TABLE test (fizz TEXT, buzz TEXT)";
+                SqliteCommand command = new SqliteCommand(commandText, con);
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public void InsertIntoTab(string entryOne, string entryTwo)
+        {
+            using (var con = new SqliteConnection("Data Source=RoejenAMS.db"))
+            {
+                con.Open();
+
+                string commandText = @"INSERT INTO test (fizz, buzz) VALUES ('"+entryOne+"','"+entryTwo+"')";
+                SqliteCommand command = new SqliteCommand(commandText, con);
+                command.ExecuteNonQuery();
+            }
+        }
+
+    }
+}    
+
---
+++ RoejenAMS/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RoejenAMS
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Button();
+        }
+
+        private void Button()
+        {
+            string hello= "";
+            DataAccess db = new DataAccess();
+            hello = db.SelectQuery();
+            B1.Content = hello;
+        }
+
+        private void B1_OnClick(object sender, RoutedEventArgs e)
+        {
+            DataAccess db = new DataAccess();
+            db.CreateTable();
+        }
+
+        private void B2_OnClick(object sender, RoutedEventArgs e)
+        {
+            string textOne = T1.Text;
+            string textTwo = T2.Text;
+            DataAccess db = new DataAccess();
+            db.InsertIntoTab(textOne, textTwo);
+        }
+
+    }
+}
